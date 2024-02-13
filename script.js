1933ecdc4c74c97eafbde1b774c8b9a22cca900c
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-14 00:09:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//complete this code
 class Rectangle {
     constructor(width, height) {
         this._width = width;
@@ -28,14 +29,6 @@ class Square extends Rectangle {
     }
 }
 
-// Test cases
-const rectangle = new Rectangle(5, 10);
-console.log(rectangle.width); // Output: 5
-console.log(rectangle.height); // Output: 10
-console.log(rectangle.getArea()); // Output: 50
-
-const square = new Square(7);
-console.log(square.width); // Output: 7
-console.log(square.height); // Output: 7
-console.log(square.getArea()); // Output: 49
-console.log(square.getPerimeter()); // Output: 28
+// Do not change the code below this line
+window.Rectangle = Rectangle;
+window.Square = Square;
